4ff3988c30c565d477d3b450be884f5b666c5bbb
Rafa Chacón <rafa.chacon@gmail.com>
2020-10-09 12:11:08 +0200

    First iteration: Added some constants to make things a bit more clear.

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -4,7 +4,17 @@ namespace Runroom\GildedRose;
 
 class GildedRose {
 
-    private $items;
+    const AGED_BRIE = "Aged Brie";
+    const BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+    const SULFURAS = "Sulfuras, Hand of Ragnaros";
+
+    const MAX_QUALITY = 50;
+    const MIN_QUALITY = 0;
+
+    /**
+     * @var Item[]
+     */
+    private array $items;
 
     function __construct($items) {
         $this->items = $items;
@@ -12,16 +22,16 @@ class GildedRose {
 
     function update_quality() {
         foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+            if ($item->name != self::AGED_BRIE && $item->name != self::BACKSTAGE) {
                 if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+                    if ($item->name != self::SULFURAS) {
                         $item->quality = $item->quality - 1;
                     }
                 }
             } else {
                 if ($item->quality < 50) {
                     $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
+                    if ($item->name == self::BACKSTAGE) {
                         if ($item->sell_in < 11) {
                             if ($item->quality < 50) {
                                 $item->quality = $item->quality + 1;
@@ -36,20 +46,20 @@ class GildedRose {
                 }
             }
 
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+            if ($item->name != self::SULFURAS) {
                 $item->sell_in = $item->sell_in - 1;
             }
 
             if ($item->sell_in < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+                if ($item->name != self::AGED_BRIE) {
+                    if ($item->name != self::BACKSTAGE) {
                         if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+                            if ($item->name != self::SULFURAS) {
                                 $item->quality = $item->quality - 1;
                             }
                         }
                     } else {
-                        $item->quality = $item->quality - $item->quality;
+                        $item->quality = 0;
                     }
                 } else {
                     if ($item->quality < 50) {
